d740d20a234bbac4b64048bcbd0e7dfdeea589c4
mariadb-0110 <mariadb0110@gmail.com>
2025-08-10 18:56:02 +0200

    add custom tables

--- src/main/java/com/luv2code/springboot/cruddemo/configuration/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/configuration/SecurityConfig.java
@@ -17,6 +17,15 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+
         return new JdbcUserDetailsManager(dataSource);
     }
 
